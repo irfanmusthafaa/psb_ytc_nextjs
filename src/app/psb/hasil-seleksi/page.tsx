4d42217d062884efa86adf9ead4f5466fb64b602
irfanmustahafaa <irfanmustafa2307@gmail.com>
2024-06-02 20:41:52 +0700

    feat: integrasi api hasil kelulusan

--- src/app/psb/hasil-seleksi/page.tsx
+++ src/app/psb/hasil-seleksi/page.tsx
@@ -1,9 +1,59 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Badge, Card, Space } from "antd";
+import { useGetProfileUser } from "@/services/user/profil/get-profil";
+
+interface ProfileData {
+  id: string;
+  status: string;
+}
 
 export default function Status() {
+  const [profile, setProfile] = useState<ProfileData | null>(null);
+
+  const { data: dataProfile, isLoading, isError } = useGetProfileUser();
+
+  useEffect(() => {
+    if (!isLoading && !isError) {
+      setProfile(dataProfile || {});
+    }
+  }, [dataProfile, isLoading, isError]);
+
+  console.log(profile?.status, "status");
+
+  const status = profile?.status;
+
+  const renderStatusRibbon = () => {
+    if (status === "lulus") {
+      return (
+        <Badge.Ribbon text="Lulus" color="green">
+          <Card title="Pemberitahuan Hasil Kelulusan" size="small">
+            Selamat Anda dinyatakan lulus dalam proses seleksi beasiswa Young
+            Tahfizh Center
+          </Card>
+        </Badge.Ribbon>
+      );
+    } else if (status === "tidak lulus") {
+      return (
+        <Badge.Ribbon text="Tidak Lulus" color="red">
+          <Card title="Pemberitahuan Status Kelulusan" size="small">
+            Mohon maaf anda dinyatakan tidak lulus dalam proses seleksi beasiswa
+            Young Tahfizh Center
+          </Card>
+        </Badge.Ribbon>
+      );
+    } else {
+      return (
+        <Badge.Ribbon text="On Proses">
+          <Card title="Pemberitahuan Status Kelulusan" size="small">
+            Saat ini masih dalam proses seleksi
+          </Card>
+        </Badge.Ribbon>
+      );
+    }
+  };
+
   return (
     <div className="bg-white h-auto min-h-[500px] m-8 box-border w-max-full rounded-xl text-black">
       <div className=" border-b border-b-gray-200 px-6 py-4 rounded-t-xl">
@@ -14,23 +64,8 @@ export default function Status() {
       <div className="pt-5 px-6">
         <div className="w-full flex justify-center items-start gap-10">
           <div className="w-full flex flex-col gap-4">
-            <Badge.Ribbon text="Lulus" color="green">
-              <Card title="Pemberitahuan Hasil Kelulusan" size="small">
-                Selamat Anda dinyatakan lulus dalam proses seleksi beasiswa
-                Young Tahfizh Center
-              </Card>
-            </Badge.Ribbon>
-            {/* <Badge.Ribbon text="Tidak Lulus" color="red">
-              <Card title="Pemberitahuan Status Kelulusan" size="small">
-                Mohon maaf anda dinyatakan tidak lulus dalam proses seleksi
-                beasiswa Young Tahfizh Center
-              </Card>
-            </Badge.Ribbon> */}
-            {/* <Badge.Ribbon text="On Proses">
-              <Card title="Pemberitahuan Status Kelulusan" size="small">
-                Saat ini masih dalam proses seleksi kelulusan
-              </Card>
-            </Badge.Ribbon> */}
+            {/* Component status */}
+            {renderStatusRibbon()}
           </div>
         </div>
       </div>
