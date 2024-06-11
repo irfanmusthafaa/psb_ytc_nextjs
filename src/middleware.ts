f57381c14dfd34c550fc346e02d3e368128b656d
irfanmustahafaa <irfanmustafa2307@gmail.com>
2024-06-11 14:41:28 +0700

    feat: add middleware user

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const cookie = cookies();
+  const token = cookie.get("TokenUser");
+
+  console.log(token, "token");
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/psb/profil",
+    "/psb/infaq",
+    "/psb/dokumen",
+    "/psb/hasil-seleksi",
+    "/psb/test-seleksi",
+    "/psb/edit-profil",
+  ],
+};
